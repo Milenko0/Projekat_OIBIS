94423617c886877c88dbe2d59f9f71535f758f01
djuradjb <bubalodjuradj@gmail.com>
2023-10-23 00:18:07 +0200

    Dodata osnova za povezivanje sa serverom

--- Projekat_12/Server/Program.cs
+++ Projekat_12/Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,12 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            using (ServiceHost host = new ServiceHost(typeof(DataManagmentServer)))
+            {
+                host.Open();
+                Console.WriteLine("Servis je uspesno pokrenut");
+                Console.ReadKey();
+            }
         }
     }
 }
