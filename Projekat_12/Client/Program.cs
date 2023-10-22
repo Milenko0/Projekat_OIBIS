2a1bf0b597710de0403b056b43afb1cda2bdcaff
Milenko0 <milenko.stankovic01@gmail.com>
2023-10-23 00:43:17 +0200

    app.config na klijentu

--- Projekat_12/Client/Program.cs
+++ Projekat_12/Client/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,10 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            ChannelFactory<IDataManagment> channel = new ChannelFactory<IDataManagment>("ServiceName");
+
+            IDataManagment proxy = channel.CreateChannel();
+            Console.ReadKey();
         }
     }
 }
