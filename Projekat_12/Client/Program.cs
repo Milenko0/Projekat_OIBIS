4d46cbc4875d641a06b78226834a7712df0ed31b
noblica06 <noblica06@gmail.com>
2023-10-30 01:03:24 +0100

    Dodao sam komunikaciju izmedju klijenta i DC

--- Projekat_12/Client/Program.cs
+++ Projekat_12/Client/Program.cs
@@ -15,7 +15,57 @@ namespace Client
             ChannelFactory<IDataManagment> channel = new ChannelFactory<IDataManagment>("ServiceName");
 
             IDataManagment proxy = channel.CreateChannel();
+
+            bool authentificated = Authentificate();
+            if (authentificated)
+            {
+                string key = RecievePrivateKey();
+                Console.WriteLine($"Primljen tajni kljuc od servisa.");
+            }
             Console.ReadKey();
+
+           
+            
+        }
+
+        private static bool Authentificate()
+        {
+            try
+            {
+                NetTcpBinding binding = new NetTcpBinding();
+                string address = "net.tcp://localhost:9999/AuthentificationService";
+
+                binding.Security.Mode = SecurityMode.Transport;
+                binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+                binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+                ChannelFactory<IAuthentificationService> authServiceFactory = new ChannelFactory<IAuthentificationService>(binding, address);
+                IAuthentificationService authentificationService = authServiceFactory.CreateChannel();
+                return authentificationService.AuthentificateClient();
+                
+            }catch(Exception e)
+            {
+                Console.WriteLine("Did not Authentificate");
+                return false;
+            }
+        }
+
+        private static string RecievePrivateKey()
+        {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:8888/TicketGrantingService";
+
+            binding.Security.Mode = SecurityMode.Transport;
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+            ChannelFactory<ITicketGrantingService> tgsFactory = new ChannelFactory<ITicketGrantingService>(binding, address);
+
+            ITicketGrantingService TGS = tgsFactory.CreateChannel();
+
+            string key = TGS.GeneratePrivateString();
+
+            return key;
         }
     }
 }
--- Projekat_12/DC/Program.cs
+++ Projekat_12/DC/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,39 @@ namespace DC
     {
         static void Main(string[] args)
         {
+            //Authentification Service setup
+            NetTcpBinding bindingAS = new NetTcpBinding();
+            string addressAS = "net.tcp://localhost:9999/AuthentificationService";
+            bindingAS.Security.Mode = SecurityMode.Transport;
+            bindingAS.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            bindingAS.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+            ServiceHost hostAS = new ServiceHost(typeof(AuthentificationService));
+            hostAS.AddServiceEndpoint(typeof(IAuthentificationService), bindingAS, addressAS);
+
+            //Ticket Granting Service setup
+            NetTcpBinding bindingTGS = new NetTcpBinding();
+            string addressTGS = "net.tcp://localhost:8888/TicketGrantingService";
+            bindingTGS.Security.Mode = SecurityMode.Transport;
+            bindingTGS.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            bindingTGS.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+            ServiceHost hostTGS = new ServiceHost(typeof(TicketGrantingService));
+            hostTGS.AddServiceEndpoint(typeof(ITicketGrantingService), bindingTGS, addressTGS);
+
+
+            //Prihvata klijenta prvo
+            hostAS.Open();
+
+           
+
+            
+            //Salje klijentu tajni kljuc
+            hostTGS.Open();
+            
+            //Ovdje treba da posalje servisu tajni kljuc isto
+
+            Console.ReadKey();
         }
     }
 }
